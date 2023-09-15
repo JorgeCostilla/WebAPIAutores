c079bab233de8106ce71bc73323a2ff4add17366
Jorge Costilla <costilla.jorgeluis@gmail.com>
2023-09-15 14:08:05 -0600

    Se vio variables de ruta clase 28

--- WebAPIAutores/Controllers/AutoresController.cs
+++ WebAPIAutores/Controllers/AutoresController.cs
@@ -20,9 +20,41 @@ namespace WebAPIAutores.Controllers
         }
 
         [HttpGet]
+        [HttpGet("listado")] // api/autores/listado
+        [HttpGet("/listado")] // listado
         public async Task<ActionResult<List<Autor>>> Get()
         {
             return await context.Autores.Include(x => x.Libros).ToListAsync();
+        }
+
+        [HttpGet("primero")] // api/autores/primero
+        public async Task<ActionResult<Autor>> PrimerAutor()
+        {
+            return await context.Autores.FirstOrDefaultAsync();
+        }
+
+        [HttpGet("{id:int}/{param2=persona}")]
+        public async Task<ActionResult<Autor>> Get(int id, string param2)
+        {
+            var autor = await context.Autores.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (autor == null)
+            {
+                return NotFound();
+            }
+            return autor;
+        }
+
+        [HttpGet("{nombre}")]
+        public async Task<ActionResult<Autor>> Get(string nombre)
+        {
+            var autor = await context.Autores.FirstOrDefaultAsync(x => x.Nombre.Contains(nombre));
+
+            if (autor == null)
+            {
+                return NotFound();
+            }
+            return autor;
         }
 
         [HttpPost]
