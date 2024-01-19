50dd63584442fe865ec4abf6c91ebeb6b8699d09
CERREY\JCostillaL <jcostillal@cerrey.com.mx>
2024-01-19 08:23:40 -0600

    Se agrego el avance de la clase de patch se hizo migracion a la bd

--- WebAPIAutores/DTOs/LibroDTO.cs
+++ WebAPIAutores/DTOs/LibroDTO.cs
@@ -7,7 +7,9 @@ namespace WebAPIAutores.DTOs
     {
         public int Id { get; set; }
         public string Titulo { get; set; }
-        
+        public DateTime FechaPublicacion { get; set; }
+
         //public List<ComentarioDTO> Comentarios { get; set; }
+
     }
 }
